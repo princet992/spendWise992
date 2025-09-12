9af400d8abd76390d6f8b0976fe8b276d6036540
Prince Thakur <princethakurameotech@gmail.com>
2025-09-12 19:12:10 +0530

    fix

--- client/src/services/expanseApi.js
+++ client/src/services/expanseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const expanseApi = createApi({
   reducerPath: "expanseData",
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_EXPANSE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_EXPANSE_URL  }),
   tagTypes: ["expanse"],
   endpoints: (builder) => ({
     //get Expanse data
