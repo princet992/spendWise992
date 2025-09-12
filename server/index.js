2a3404bc306eebb6d985629f6e6f6b9432b7cb81
Prince Thakur <princethakurameotech@gmail.com>
2025-09-12 19:14:58 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:"https://expansetracker992.netlify.app/" || "http://localhost:5173"
+}));
 
 //register User
 app.post("/userAuth", registerUser);
